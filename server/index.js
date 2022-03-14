4b171428f4224811574a9800863ca3cb8980dfb1
John Danlami <johnsdanlami@gmail.com>
2022-03-15 00:16:02 +0100

    create user api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
 dotenv.config();
 app.use(express.json());
 mongoose
@@ -10,7 +11,8 @@ mongoose
   .then(console.log("connected to mongodb"))
   .catch((err) => console.log(err));
 
-app.use("/api", authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
 
 app.listen(5000, () => {
   console.log("connected to port 5000");
---
+++ server/routes/users.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const { rawListeners } = require("../models/User");
+
+const Post = require("../models/Post");
+// UPDATE
+router.put("/:id", async (req, res) => {
+  const reqBody = req.body;
+  if (reqBody.userId === req.params.id) {
+    if (reqBody.password) {
+      const salt = await bcrypt.genSalt(10);
+      reqBody.password = await bcrypt.hash(reqBody.password, salt);
+    }
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: reqBody,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.json("you can only update your account");
+  }
+});
+// DELETE
+router.delete("/:id", async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+  if (req.body.userId === req.params.id) {
+    if (user) {
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted ....");
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(404).json("User not found");
+    }
+  } else {
+    res.status(401).json("You can only delete your account");
+  }
+});
+// GET ALL
+// GET ONE
+
+module.exports = router;
