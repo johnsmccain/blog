d776348811fc4ae8ab0bf7fb87bdf7a61b388c87
John Danlami <johnsdanlami@gmail.com>
2022-03-18 19:25:38 +0100

    Create, Read, Update and Delete Post api

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 dotenv.config();
 app.use(express.json());
 mongoose
@@ -13,6 +15,8 @@ mongoose
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/post", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(5000, () => {
   console.log("connected to port 5000");
